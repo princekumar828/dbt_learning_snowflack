a7e07d4a429ba98857d4614dec6e5dcf2305f652
princekumar828 <kumarprince282002@gmail.com>
2025-05-19 00:36:41 +0530

    fix symentic

--- models/marts/core/order_metrics.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    COUNT(*) AS total_orders,
-    AVG(order_value) AS average_order_value,
-    SUM(CASE WHEN status = 'completed' THEN 1 ELSE 0 END) AS completed_orders,
-    SUM(CASE WHEN status = 'returned' THEN 1 ELSE 0 END) AS returned_orders
-FROM
-    {{ ref('stg_orders') }};
